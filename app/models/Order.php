8886156ec5b7b3a0a76f6e2b61f10d562ba3f0ad
Lancevrot <33875002+Lancevrot@users.noreply.github.com>
2018-08-13 15:25:43 +0300

    Add mailing functions

---
+++ app/models/Order.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: avolo
+ * Date: 13-Aug-18
+ * Time: 14:11
+ */
+
+namespace app\models;
+
+
+class Order
+{
+
+}
---
+++ app/views/Cart/view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: avolo
+ * Date: 13-Aug-18
+ * Time: 13:49
+ */
