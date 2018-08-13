973514cde81cf65c1a1b2cc341126c37d3d0f430
Lancevrot <33875002+Lancevrot@users.noreply.github.com>
2018-08-13 15:25:54 +0300

    Add mailing functions

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -3,6 +3,8 @@
 namespace app\controllers;
 
 use app\models\Cart;
+use app\models\Order;
+use app\models\User;
 
 class CartController extends AppController {
 
@@ -52,6 +54,46 @@ class CartController extends AppController {
         unset($_SESSION['cart.sum']);
         unset($_SESSION['cart.currency']);
         $this->loadView('cart_modal');
+    }
+
+    public function viewAction()
+    {
+        $this->setMeta('Корзина');
+    }
+
+    public function checkoutAction()
+    {
+        if(!empty($_POST)){
+            // Rigistration of the user
+            if(!User::checkAuth()){
+                $user = new User();
+                $data = $_POST;
+                $user->load($data);
+                if(!$user->validate($data) || !$user->checkUnique()){
+                    $user->getErrors();
+                    $_SESSION['form-data'] = $data;
+                    redirect();
+
+                } else {
+                    $user->attributes['password'] = password_hash($user->attributes['password'], PASSWORD_DEFAULT);
+                    if(!$user_id = $user->save('user')){
+                        $_SESSION['success'] = 'Ошибка!';
+                        redirect();
+                    }
+
+                }
+
+            }
+
+            //Savin order
+            $data['user_id'] = isset($user_id) ? $user_id : $_SESSION['user']['id'];
+            $data['note'] = !empty($_POST['note']) ? $_POST['note'] : '';
+            $user_email = isset($_SESSION['user']['email']) ? $_SESSION['user']['email'] : $_POST['email'];
+            $order_id = Order::saveOrder($data);
+            Order::mailOrder($order_id, $user_email);
+
+        }
+        redirect();
     }
 
 }
--- config/params.php
+++ config/params.php
@@ -1,9 +1,12 @@
 <?php
 
 return [
-    'admin_email' => 'admin@mail.com',
+    'admin_email' => 'avoloshin225@gmail.com',
     'shop_name' => 'Магазин ishop2.loc',
     'pagination' => 3,
-    'smtp_login' => '',
-    'smtp_password' => '',
+    'smtp_login' => 'avoloshin225@ukr.net',
+    'smtp_host' => 'smtp.ukr.net',
+    'smtp_port' => '2525',
+    'smtp_protocol' => 'ssl',
+    'smtp_password' => 'Curson123',
 ];
